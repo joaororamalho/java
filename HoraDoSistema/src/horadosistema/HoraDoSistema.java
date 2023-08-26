f11a915cf572703a03ef0ac37377f4634e1c8b1b
joaororamalho <devjoaororamalho@gmail.com>
2023-08-26 11:16:15 -0300

    Update HoraDoSistema.java

--- HoraDoSistema/src/horadosistema/HoraDoSistema.java
+++ HoraDoSistema/src/horadosistema/HoraDoSistema.java
@@ -18,7 +18,7 @@ public class HoraDoSistema {
     public static void main(String[] args) {
         // TODO code application logic here
         Date relogio = new Date();
-        System.out.println("A hora do sitema é");
+        System.out.println("A hora do sitema é:");
         System.out.println(relogio.toString());
     }
     
