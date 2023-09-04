7714bf24d32e5e49f6c55bf43fc57bdc63a79e43
joaororamalho <devjoaororamalho@gmail.com>
2023-09-04 20:08:37 -0300

    Update OperadoresAritmeticos.java

--- OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -19,6 +19,12 @@ public class OperadoresAritmeticos {
         int n2 = 5;
         float m = (n1 + n2)/2;
         System.out.println("A média é igual a " + m);
+        
+        int numero = 5;
+        int valor = 5 + numero++;
+        System.out.println(valor);
+        System.out.println(numero);
+        int x
     }
     
 }
