c8f2eddfd15656940d6b9df848b5a8ddfa894376
joaororamalho <devjoaororamalho@gmail.com>
2023-09-12 19:38:07 -0300

    create verificadoridade

--- OperadoresLogicos/src/operadoreslogicos/OperadoresLogicos.java
+++ OperadoresLogicos/src/operadoreslogicos/OperadoresLogicos.java
@@ -15,6 +15,13 @@ public class OperadoresLogicos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int x, y, z;
+        x = 4;
+        y = 7;
+        z = 12;
+        boolean r;
+        r = (x<y && y<z)?true:false;
+        System.out.println(r);
     }
     
 }
