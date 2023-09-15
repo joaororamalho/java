c21d4146a01c38386a02fd4f4f37f109e7dc292b
joaororamalho <devjoaororamalho@gmail.com>
2023-09-15 19:15:57 -0300

    create ProgramaPernas

---
+++ ProgramaPernas/src/programapernas/ProgramaPernas.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programapernas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author theak
+ */
+public class ProgramaPernas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner tec = new Scanner(System.in);
+        System.out.println("Quantas pernas você tem?");
+        int perna = tec.nextInt();
+        String tipo;
+        System.out.println("Isso é um(a) ");
+        switch (perna) {
+            case 1:
+                tipo = "Saci";
+                break;
+            case 2:
+                tipo = "Bípede";
+                break;
+            case 4:
+                tipo = "Quadrúpede";
+                break;
+            case 6,8:
+                tipo = "Aranha";
+                break;
+            default:
+                tipo = "ET";
+        }
+        System.out.println(tipo);
+    }
+    
+}
