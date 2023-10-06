6c04c157ffb3432261026b49b6f9360eb0d4a14a
joaororamalho <devjoaororamalho@gmail.com>
2023-10-06 19:08:33 -0300

    update TesteFuncao02

--- TesteFuncao02/src/testefuncao02/Operacoes.java
+++ TesteFuncao02/src/testefuncao02/Operacoes.java
@@ -10,4 +10,12 @@ package testefuncao02;
  */
 public class Operacoes {
     
+    public static String contador(int i, int f) {
+        String s = "";
+        for (int c = i; c <= f; c++) {
+            s += c + " ";
+        }
+        return s;
+    }
+    
 }
