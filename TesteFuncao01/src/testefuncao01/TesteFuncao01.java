43c4a14650f7d56fe219d676f1a49e70081b9af4
joaororamalho <devjoaororamalho@gmail.com>
2023-10-06 19:00:42 -0300

    Update TesteFuncao01.java

--- TesteFuncao01/src/testefuncao01/TesteFuncao01.java
+++ TesteFuncao01/src/testefuncao01/TesteFuncao01.java
@@ -14,15 +14,16 @@ public class TesteFuncao01 {
      * @param args the command line arguments
      */
     
-    static void soma(int a, int b) {
+    static int soma(int a, int b) {
         int s = a + b;
-        System.out.println("A soma é: " + s);
+        return s;
     }
     
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Começou o programa");
-        soma(5,2);
+        int sm = soma(5,2);
+        System.out.println("A soma vale " + sm);
     }
     
 }
