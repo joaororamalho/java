a09b76e9f2759793dedd65afaf781f1146c9d868
joaororamalho <devjoaororamalho@gmail.com>
2023-10-11 19:46:12 -0300

    Update Aula02.java

--- Aula02/src/aula02/Aula02.java
+++ Aula02/src/aula02/Aula02.java
@@ -7,8 +7,15 @@ public class Aula02 {
         c1.cor = "Azul";
         c1.ponta = 0.5f;
         c1.tampar();
-        c1.rabiscar();
         c1.status();
+        c1.rabiscar();
+        
+        Caneta c2 = new Caneta();
+        c2.modelo = "Hostnet";
+        c2.cor = "Preta";
+        c2.destampar();
+        c2.status();
+        c2.rabiscar();
     }
     
 }
